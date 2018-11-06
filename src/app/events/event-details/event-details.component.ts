2cc3f58355316f6738543a4aa6f24068e223e4db
Hamed <hamedsalami@gmail.com>
2018-11-06 05:55:14 +0200

    study

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -13,6 +13,8 @@ import { IEvent, ISession } from '../shared';
 export class EventDetailsComponent implements OnInit {
     event: IEvent;
     addMode: boolean;
+    filterBy = 'all';
+    sortBy: 'votes';
 
     constructor(private eventService: EventsService,
         private activatedRoute: ActivatedRoute) { }
